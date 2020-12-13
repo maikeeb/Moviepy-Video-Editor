eec44cf78d021cdeb93b9500c2a1a09861c84baf
maikeeb <leaguehomey@gmail.com>
2020-12-13 18:08:35 -0500

    allowed the support of h264 if a supported graphics card is detected 0-15% faster

--- scripts/export.py
+++ scripts/export.py
@@ -4,9 +4,16 @@ from moviepy.editor import *
 def render(videos, name):
     L = [video.render() for video in videos]
     composite = [CompositeVideoClip(L)]
-    concatenate_videoclips(composite, padding=-1).write_videofile(
-        "..\\finalVideos\\%s.mp4" % name,
-        remove_temp=True, threads=4)
+    try:
+        concatenate_videoclips(composite, padding=-1).write_videofile(
+            "..\\finalVideos\\%s.mp4" % name, codec='h264',
+            remove_temp=True, threads=4)
+    # known issue: increasing threads above 4 slows rendering
+    except Exception as e:
+        print("You don't have a nvidia graphics card")
+        concatenate_videoclips(composite, padding=-1).write_videofile(
+            "..\\finalVideos\\%s.mp4" % name,
+            remove_temp=True, threads=4)
 
 
 if __name__ == "__main__":
@@ -18,4 +25,4 @@ if __name__ == "__main__":
         "..\\finalVideos\\%s.mp4" % "name",
         remove_temp=True, threads=4)"""
     # this one to
-    render([Clip('../finalVideos/clip1.mp4')], "hello")
+    render([Clip('../currentVideos/40hiko0.mp4')], "hello")
